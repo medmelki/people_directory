689c22f876d34fa8bc2d1252512440cc507c27dc
Melki <m.mohamedmelki@gmail.com>
2015-07-20 00:53:03 +0100

    add rest consuming & enhance the layout

---
+++ js/companyCtrl.js
@@ -0,0 +1,91 @@
+
+app.controller("companyCtrl", function ($scope, $http) {
+
+    $http.get("http://localhost:8080/peopledirectory/rest/company/all")
+        .success(function (response) {
+            $scope.companies = response;
+            $scope.setNextEmployeeId();
+        });
+
+
+
+    $scope.setDepartments = function (x) {
+        $scope.departments = x.departments;
+        $scope.activeCompany = x;
+    };
+
+    $scope.setEmployees = function (x, index) {
+        $scope.employees = x.employees;
+        $scope.activeDepartment = x;
+        $scope.activeDepartmentId = index + 1;
+    };
+
+    $scope.setActiveEmployee = function (x) {
+        $scope.activeEmployee = x;
+    };
+
+    $scope.addEmployee = function (companyName, departmentId, name) {
+
+        $http.get("http://localhost:8080/peopledirectory/rest/company/employee/add/" + companyName + "/" + departmentId + "/" + $scope.nextemployeeId + "/" + name)
+            .success(function (response) {
+                $scope.deleteEmployeeSuccess = response;
+            });
+    }
+
+    $scope.deleteEmployee = function () {
+
+        $http.get("http://localhost:8080/peopledirectory/rest/company/employee/delete/" + $scope.activeCompany.name + "/" + new Number($scope.activeDepartmentId) + "/" + $scope.activeEmployee.name)
+            .success(function (response) {
+                $scope.deleteEmployeeSuccess = response;
+            });
+    }
+
+    $scope.deleteDepartment = function () {
+
+        $http.get("http://localhost:8080/peopledirectory/rest/company/department/delete/" + $scope.activeCompany.name + "/" + $scope.activeDepartment.name)
+            .success(function (response) {
+                $scope.deleteEmployeeSuccess = response;
+            });
+    }
+
+    $scope.deleteCompany = function () {
+
+        $http.get("http://localhost:8080/peopledirectory/rest/company/delete/" + $scope.activeCompany.name)
+            .success(function (response) {
+                $scope.deleteEmployeeSuccess = response;
+            });
+    }
+
+    $scope.deleteItem = function (modelItem) {
+
+        switch (modelItem) {
+
+            case "company":
+                $scope.deleteCompany();
+                break;
+            case "department":
+                $scope.deleteDepartment();
+                break;
+            case "employee":
+                $scope.deleteEmployee();
+                break;
+        }
+    }
+
+    $scope.setNextEmployeeId = function () {
+        $scope.nextemployeeId = 0;
+        for (var cmpItem in $scope.companies) {
+
+            for (var i in $scope.companies[cmpItem].departments) {
+
+                for (var j in $scope.companies[cmpItem].departments[i].employees) {
+
+                    for (var k in $scope.companies[cmpItem].departments[i].employees[j]) {
+                        $scope.nextemployeeId = (new Number($scope.nextemployeeId) + 1);
+                    }
+                }
+            }
+        }
+    }
+
+});
--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 
+
+function setActive(that, category){
+    that.className += " info";
+    $('.'+ category + 'Element').not(that).removeClass("info");
+}
